222e631f766652f8fcaacd6e5f7806ac053fd1a7
Eric_Lian <git@superexercisebook.com>
2020-05-03 10:25:28 +0800

    完善帖子引用提示

--- app/Paraparty/References/References.php
+++ app/Paraparty/References/References.php
@@ -12,11 +12,22 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class References
 {
-
+    /**
+     * 获取引用机器人的 UID
+     *
+     * @return int
+     * @throws BindingResolutionException
+     */
     private static function reference_bot_user_id(){
         return (int)(ParaConfig::get("paraparty.references.bot_user_id", null));
     }
 
+    /**
+     * 在帖子中识别帖子地址
+     *
+     * @param string $data
+     * @return string|string[]|null
+     */
     public static function detecting(string $data)
     {
         try {
@@ -36,6 +47,17 @@ class References
         return preg_replace($pattern, $replacement, $data);
     }
 
+    /**
+     * 插入引用信息
+     *
+     * @param ThreadRepository $threads
+     * @param User $actor
+     * @param string $content
+     * @param int $tid
+     * @param int $post_id
+     * @param $ip
+     * @throws BindingResolutionException
+     */
     public static function update(ThreadRepository $threads, User $actor, string $content, int $tid, int $post_id, $ip)
     {
         try {
@@ -51,6 +73,17 @@ class References
             self::add_reference($threads, $actor, (int)$mentioned_tid, $tid, $post_id, $ip);
     }
 
+    /**
+     * 插入引用信息
+     *
+     * @param ThreadRepository $threads
+     * @param User $actor
+     * @param int $mentioned_tid
+     * @param int $tid
+     * @param int $post_id
+     * @param $ip
+     * @throws BindingResolutionException
+     */
     private static function add_reference(ThreadRepository $threads, User $actor, int $mentioned_tid, int $tid, int $post_id, $ip){
         if ($mentioned_tid == $tid) return;
 
@@ -60,7 +93,7 @@ class References
             return;
         }
 
-        $content = json_encode((object)["type"=>"reference", "tid"=>$tid, "post_id"=>$post_id]);
+        $content = json_encode((object)["type"=>"reference", "tid"=>$tid, "post_id"=>$post_id, "operator"=>$actor->id]);
 
         $reference = Post::reply(
             $mentioned_tid,
@@ -68,11 +101,36 @@ class References
             self::reference_bot_user_id(),
             $ip,
             null,
-            null,
+            $actor->id,
             false,
             false
         );
         $reference->save();
+    }
+
+    /**
+     * 获取引用操作者信息
+     *
+     * @param $post
+     * @throws BindingResolutionException
+     */
+    public static function get_references_info(&$post)
+    {
+        if ($post['user_id'] == self::reference_bot_user_id()) {
+
+            $content = $post['content'];
+
+            $obj = json_decode($content, true);
+
+            $operator = User::find($obj['operator']);
+
+            //$relations = $post->getRelations();
+            //array_push($relations, ["operator" => $operator] );
+            //$post->setRelations($relations);
+            $post->setRelation("operator", $operator);
+        } else {
+            $post->setRelation("operator", $post->getRelation("user"));
+        }
     }
 
 }
