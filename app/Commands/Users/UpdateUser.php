7b221bd87679c918faa0061e02caa6ad779a3a44
Eric_Lian <git@superexercisebook.com>
2020-05-22 16:05:29 +0800

    DiscuzQ v0.5.200522

--- app/Commands/Users/UpdateUser.php
+++ app/Commands/Users/UpdateUser.php
@@ -25,6 +25,7 @@ use Discuz\Foundation\EventsDispatchTrait;
 use Discuz\SpecialChar\SpecialCharServer;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
 
 class UpdateUser
@@ -74,6 +75,7 @@ class UpdateUser
      */
     public function __invoke()
     {
+        /** @var User $user */
         $user = $this->users->findOrFail($this->id, $this->actor);
 
         $isSelf = $this->actor->id === $user->id;
@@ -158,6 +160,12 @@ class UpdateUser
             OperationLog::writeLog($this->actor, $user, $actionType, $logMsg);
         }
 
+        if ($expiredAt = Arr::get($this->data, 'data.attributes.expired_at')) {
+            $this->assertAdmin($this->actor);
+
+            $user->expired_at = Carbon::parse($expiredAt);
+        }
+
         if ($groups = Arr::get($attributes, 'groupId')) {
             $this->assertCan($this->actor, 'edit.group', $user);
 
@@ -211,7 +219,10 @@ class UpdateUser
             $user->changeUsername($username);
         }
 
-        if ($signature = Arr::get($attributes, 'signature')) {
+        if (Arr::has($attributes, 'signature')) {
+            // 可为空
+            $signature = Arr::get($attributes, 'signature');
+
             // 敏感词校验
             $this->censor->checkText($signature);
             if ($this->censor->isMod) {
