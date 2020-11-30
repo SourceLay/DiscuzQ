caa8bf0316e72e24cf80773a19b8b45533563740
Eric_Lian <git@superexercisebook.com>
2020-11-30 17:19:10 +0800

    完成帖子与分享之间的关联

---
+++ app/SourceLay/Listeners/FileShareBBCode.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\SourceLay\Listeners;
+
+use App\Events\Post\Revised as PostRevised;
+use App\Events\Post\Saved as PostSaved;
+use App\SourceLay\Models\FileShare;
+use App\SourceLay\Models\FileThread;
+use Illuminate\Contracts\Events\Dispatcher;
+
+class FileShareBBCode
+{
+    public function subscribe(Dispatcher $events){
+        $events->listen(PostRevised::class, [$this, 'whenPostRevised']);
+    }
+
+    public function whenPostRevised(PostSaved $event){
+        $actor = $event->actor;
+        $post = $event->post;
+        $thread = $post->thread;
+        $content = $post->content;
+
+        if (preg_match_all("/\[fileshare\](?<fileshare_id>\d+)\[\/fileshare\]/", $content, $matches))
+        {
+            $fileshare_ids = $matches['fileshare_id'];
+            foreach ($fileshare_ids as $fileshrae_id) {
+                $fileShare = FileShare::find($fileshrae_id);
+
+                $fileThread = new FileThread();
+                if ($actor->id == $fileShare->id) {
+                    $fileThread->priority = 100;
+                } else if ($fileShare->shared_type != FileShare::FILESHARE_TYPE_FREE) {
+                    $fileThread->priority = 10;
+                } else {
+                    continue
+                }
+
+                $fileThread->file_id = $fileShare->file->id;
+                $fileThread->post_id = $post->id;
+                $fileThread->thread_id = $thread->id;
+                $fileThread->save();
+            }
+        }
+
+    }
+}
---
+++ app/SourceLay/Models/FileThread.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\SourceLay\Models;
+
+use App\Models\Post;
+use App\Models\Thread;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+/**
+ * @property int id
+ * @property int file_id
+ * @property int post_id
+ * @property int priority
+ * @property int thread_id
+ * @property Carbon created_at
+ * @package App\Models
+ * @package App\SourceLay\Models
+ */
+class FileThread extends Model
+{
+    protected $table = 'sourcelay_file_thread_relation_info';
+
+    protected $primaryKey = 'id';
+
+    protected $keyType = 'int';
+
+    const UPDATED_AT = null;
+
+    const FILE_STATUS_PENDING = 0;
+
+    const FILE_STATUS_COMMITED = 1;
+
+    function file()
+    {
+        return $this->hasOne(File::class, 'id', 'file_id');
+    }
+
+    function post()
+    {
+        return $this->hasOne(Post::class, 'id', 'post_id');
+    }
+
+    function thread()
+    {
+        return $this->hasOne(Thread::class, 'id', 'thread_id');
+    }
+}
